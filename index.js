be601f850c13d25a8bc1f4d9c50168cf366f28d5
William Decker <willdecker@gmail.com>
2018-06-19 05:28:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,22 +4,14 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-happyHolidays();
-
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-
-happyHolidaysTo("Lucy");
 
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-happyHolidayTo("Halloween", "Ben");
-
 function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}!`
 }
-
-holidayCountdown(11, "Christmas");
